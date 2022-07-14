566100a44617b61ca920a68b5f22db61dbb925a8
paweluz <paweluz@o2.pl>
2022-07-14 18:15:49 +0200

    Refactoring of packages

---
+++ Java/src/main/java/pl/pawlak/pawel/codewars/kata/accumul/Accumul.java
@@ -0,0 +1,23 @@
+package pl.pawlak.pawel.codewars.kata.accumul;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Accumul {
+
+  public static String accum(final String text) {
+    if (text == null) {
+      throw new IllegalArgumentException();
+    }
+    return IntStream.range(0, text.length())
+        .mapToObj(i -> singleCharToWord(text.charAt(i), i))
+        .collect(Collectors.joining("-"));
+  }
+
+  private static String singleCharToWord(final char charAt, final int index) {
+    return IntStream.range(0, index + 1)
+        .mapToObj(i -> i == 0 ? String.valueOf(charAt).toUpperCase() : String.valueOf(charAt).toLowerCase())
+        .collect(Collectors.joining());
+  }
+
+}
---
+++ Java/src/main/java/pl/pawlak/pawel/codewars/kata/greed/Greed.java
@@ -0,0 +1,31 @@
+package pl.pawlak.pawel.codewars.kata.greed;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Greed {
+
+  static Map<Integer, Function<Long, Long>> values = new HashMap<>();
+
+  static {
+    values.put(1, elem -> (elem / 3) * 1000 + (elem % 3) * 100);
+    values.put(2, elem -> (elem / 3) * 200);
+    values.put(3, elem -> (elem / 3) * 300);
+    values.put(4, elem -> (elem / 3) * 400);
+    values.put(5, elem -> (elem / 3) * 500 + (elem % 3) * 50);
+    values.put(6, elem -> (elem / 3) * 600);
+  }
+
+  public static int greedy(int[] dice) {
+    final Map<Integer, Long> collect = Arrays.stream(dice).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    return collect.entrySet().stream()
+        .map(elem -> values.get(elem.getKey()).apply(elem.getValue()))
+        .mapToInt(Long::intValue)
+        .sum();
+
+  }
+
+}
