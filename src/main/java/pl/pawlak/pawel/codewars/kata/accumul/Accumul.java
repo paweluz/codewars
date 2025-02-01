a50c9e228c99c953d0d66a4a783427c7f792a715
paweluz <paweluz@o2.pl>
2025-02-01 15:36:12 +0100

    Refactoring,simplify

--- src/main/java/pl/pawlak/pawel/codewars/kata/accumul/Accumul.java
+++ src/main/java/pl/pawlak/pawel/codewars/kata/accumul/Accumul.java
@@ -5,19 +5,19 @@ import java.util.stream.IntStream;
 
 public class Accumul {
 
-  public static String accum(final String text) {
-    if (text == null) {
-      throw new IllegalArgumentException();
+    public static String accum(final String text) {
+        if (text == null) {
+            throw new IllegalArgumentException();
+        }
+        return IntStream.range(0, text.length())
+                .mapToObj(i -> singleCharToWord(text.charAt(i), i))
+                .collect(Collectors.joining("-"));
     }
-    return IntStream.range(0, text.length())
-        .mapToObj(i -> singleCharToWord(text.charAt(i), i))
-        .collect(Collectors.joining("-"));
-  }
 
-  private static String singleCharToWord(final char charAt, final int index) {
-    return IntStream.range(0, index + 1)
-        .mapToObj(i -> i == 0 ? String.valueOf(charAt).toUpperCase() : String.valueOf(charAt).toLowerCase())
-        .collect(Collectors.joining());
-  }
+    private static String singleCharToWord(final Character charAt, final int index) {
+        return charAt.toString().toUpperCase() + IntStream.range(1, index + 1)
+                .mapToObj(i -> String.valueOf(charAt).toLowerCase())
+                .collect(Collectors.joining());
+    }
 
 }
--- src/test/java/pl/pawlak/pawel/codewars/kata/accumul/AccumulTest.java
+++ src/test/java/pl/pawlak/pawel/codewars/kata/accumul/AccumulTest.java
@@ -1,60 +1,31 @@
 package pl.pawlak.pawel.codewars.kata.accumul;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
-
 public class AccumulTest {
 
-  private String input;
+    private String input;
 
-  @Test
-  public void testWhenGivenNullAsInput() {
-    assertThrows(IllegalArgumentException.class, () -> {
-      Accumul.accum(null);
-    });
-  }
+    @Test
+    public void testWhenGivenNullAsInput() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Accumul.accum(null);
+        });
+    }
 
-  @Test
-  public void testEmptyString(){
-    When:
-    input = "";
-
-    Then:
-    assertEquals("", Accumul.accum(input));
-  }
-
-
-  @Test
-  public void testSingleCharacter(){
-    When:
-    input = "a";
-
-    Then:
-    assertEquals("A", Accumul.accum(input));
-  }
-
-  @Test
-  public void testTwoCharacters(){
-    When:
-    input = "ab";
-
-    Then:
-    assertEquals("A-Bb", Accumul.accum(input));
-  }
-
-  private static void testing(String actual, String expected) {
-    assertEquals(expected, actual);
-  }
-  @Test
-  public void testMany() {
-    System.out.println("Fixed Tests accum");
-    testing(Accumul.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
-    testing(Accumul.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
-    testing(Accumul.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
-    testing(Accumul.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
-    testing(Accumul.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
-  }
+    @Test
+    public void test() {
+        assertEquals("", Accumul.accum(""));
+        assertEquals("A", Accumul.accum("a"));
+        assertEquals("A-Bb", Accumul.accum("ab"));
+        assertEquals("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu", Accumul.accum("ZpglnRxqenU"));
+        assertEquals("N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb", Accumul.accum("NyffsGeyylB"));
+        assertEquals("M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu", Accumul.accum("MjtkuBovqrU"));
+        assertEquals("E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm", Accumul.accum("EvidjUnokmM"));
+        assertEquals("H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc", Accumul.accum("HbideVbxncC"));
+    }
 
 }
