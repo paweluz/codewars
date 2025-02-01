6dd671572dee76a58a2fd51c2eae9e63c30c09fa
paweluz <paweluz@o2.pl>
2025-02-01 15:20:06 +0100

    Package refactoring

---
+++ src/test/java/pl/pawlak/pawel/codewars/kata/Test.java
@@ -0,0 +1,9 @@
+package pl.pawlak.pawel.codewars.kata;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "animals";
+        System.out.println(s.charAt(7));
+    }
+}
